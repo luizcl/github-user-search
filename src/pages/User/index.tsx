b28e400e2623d8fc69d908aa3f48baa3cb9d4393
Luiz Claudio Moura Moraes <luizmoraes@demprel.local>
2020-10-22 10:34:31 -0300

    Input com defeito

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Button from '../../core/components/Button';
 import './styles.scss'
 
 const User = () => {
 
-    const Card = () => (
+    const [userName, setUserName] = useState('');
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserName(event.target.value);
+        console.log(userName)
+    }
+
+    const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    }
+
+    const CardSearch = () => (
+        <form onSubmit={handleOnSubmit}>
         <div className="card-form card-style">
-            <div>
                 <h1>
                     Encontre um perfil Github
                 </h1>
                 <input
-                    className="input-style">
+                    className="input-user-style"
+                    type="text"
+                    value={userName}
+                    onChange={handleOnChange}
+                    placeholder="Nome do Usuário">
                 </input>
-            </div>
-            <Button text="Encontrar" to="/user/:userName" className="btn-user-change"/>
+            {/*<Button text="Encontrar" to={`/user/${userName}`} className="btn-user-change"/>*/}
         </div>
+        </form>
     );
     
+    const CardDetails = () => (
+        <div>
+
+        </div>
+    );
+
     return (
         <BrowserRouter>
-            <Card />
+            <CardSearch />
             <Switch>
                 <Route path="/user/:userName">
-
+                    <CardDetails />
                 </Route>
             </Switch>
         </BrowserRouter>
