3ae88aba9250690bb15ef013dacc76b527781884
Luiz Claudio Moura Moraes <luizmoraes@demprel.local>
2020-10-23 10:22:31 -0300

    Card Details - Status Bug

--- src/pages/User/CardDetails/index.tsx
+++ src/pages/User/CardDetails/index.tsx
@@ -2,47 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import './styles.scss'
+import ImageLoader from '../components/Loaders/imageLoader';
+import InfoLoader from '../components/Loaders/infoLoader';
 
 type ParamsType = {
     userName: string;
 }
 
 type User = {
-    "login": "luizcl",
-    "id": 48841821,
-    "node_id": "MDQ6VXNlcjQ4ODQxODIx",
-    avatar_url: string,
-    "gravatar_id": "",
-    url: string,
-    "html_url": "https://github.com/luizcl",
-    "followers_url": "https://api.github.com/users/luizcl/followers",
-    "following_url": "https://api.github.com/users/luizcl/following{/other_user}",
-    "gists_url": "https://api.github.com/users/luizcl/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/luizcl/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/luizcl/subscriptions",
-    "organizations_url": "https://api.github.com/users/luizcl/orgs",
-    "repos_url": "https://api.github.com/users/luizcl/repos",
-    "events_url": "https://api.github.com/users/luizcl/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/luizcl/received_events",
-    "type": "User",
-    "site_admin": false,
-    "name": "Luiz",
-    company: string,
-    blog: string,
-    location: string,
-    "email": null,
-    "hireable": true,
-    "bio": "Still learning...",
-    "twitter_username": null,
-    public_repos: number,
-    "public_gists": 0,
-    followers: number,
-    following: number,
-    created_at: Date,
-    "updated_at": "2020-10-22T01:00:26Z"
-}
-
-type User_teste = {
     name: string,
     avatar_url: string,
     url: string,
@@ -58,8 +25,8 @@ type User_teste = {
 const CardDetails = () => {
 
     const { userName } = useParams<ParamsType>();
-    const [user, setUser] = useState<User_teste>();
-    const [status, setStatus] = useState(0);
+    const [user, setUser] = useState<User>();
+    const [statusOk, setStatusOk] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const BASE_URL = 'https://api.github.com/users/';
 
@@ -71,16 +38,50 @@ const CardDetails = () => {
             url: BASE_URL + userName,
         })
             .then((response) => {
+                console.log("Deu bom");
                 console.log(response);
-                setStatus(response.status);
+                setStatusOk(response.status === 200);
+                console.log(statusOk);
                 setUser(response.data);
             })
-            .finally(() => setIsLoading(false));
+            .finally(() => {
+                setIsLoading(false);
+                if(!statusOk){
+                    console.log("Deu ruim")
+                    setUser({
+                        name : "Usuário não encontrado",
+                        avatar_url: "https://neilpatel.com/wp-content/uploads/2019/05/ilustracao-panda-com-problema-error-404-not-found.jpeg",
+                        url: "",
+                        company: "",
+                        blog: "",
+                        location: "",
+                        public_repos: 0,
+                        followers: 0,
+                        following: 0,
+                        created_at: new Date("0000-00-00T00:00:00Z"),
+                    });
+                }
+                console.log("Passou aqui");
+                console.log(user);
+
+            });
     }, [userName]);
 
     return (
         <div className="card-style card-details-form">
-            <img src={user?.avatar_url} alt={user?.name} className="img-card-details"/>
+            { isLoading ? 
+                        <>
+                            <div className="img-item">
+                                <ImageLoader /> 
+                            </div>
+                            <div className="info-item">
+                                <InfoLoader />
+                            </div>
+                        </> : 
+                        <>
+                            <img src={user?.avatar_url} alt={user?.name} className="img-card-details" />
+                        </>
+            }
         </div>
     );
 }
